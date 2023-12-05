8db39df508786c77d5851de3372fd08c6ca7f37f
NoobTenLuka <NoobTenLuka@gmail.com>
2023-12-05 14:08:30 +0100

    Add an idea of a better solution to day 5

--- src/day05.rs
+++ src/day05.rs
@@ -37,6 +37,7 @@ pub fn part1(input: String) -> String {
     seeds.iter().min().unwrap().to_string()
 }
 
+// Please use --release thx
 pub fn part2(input: String) -> String {
     let mut operations = input.split("\n\n");
     let seeds = operations
@@ -71,12 +72,58 @@ pub fn part2(input: String) -> String {
             }
         }
         for (start, len) in &seeds {
-           let end = start + len;
-           if current_dest >= *start && current_dest < end {
+            let end = start + len;
+            if current_dest >= *start && current_dest < end {
                 return x.to_string();
-           }
+            }
         }
     }
 
     0.to_string()
+}
+
+// Unfinished, but may result in a better runtime once finished
+#[allow(dead_code)]
+pub fn part2_idea(input: String) -> String {
+    let mut operations = input.split("\n\n");
+    let mut seeds = operations
+        .next()
+        .unwrap()
+        .split_once(":")
+        .unwrap()
+        .1
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<i64>().unwrap())
+        .tuples::<(_, _)>()
+        .map(|(start, len)| (start, start + len))
+        .collect_vec();
+
+    operations.for_each(|operation| {
+        let mut new_ranges = Vec::new();
+        operation.split("\n").skip(1).for_each(|op_line| {
+            let (dest, source_start, range) = op_line
+                .split_ascii_whitespace()
+                .map(|x| x.parse::<i64>().unwrap())
+                .next_tuple()
+                .unwrap();
+            let source_end = source_start + range;
+            let diff = source_start - dest;
+            for (start, end) in &seeds {
+                if source_start >= *start && source_end <= *end {
+                    // Equal or smaller range
+                    new_ranges.push((source_start - diff, source_end - diff));
+                } else if source_start < *end && source_start >= *start {
+                    println!("{start} {end} {source_start} {source_end} {diff}");
+                    new_ranges.push((source_start - diff, end - diff));
+                } else if source_end > *start && source_start < *start {
+                    new_ranges.push((start - diff, source_end - diff));
+                }
+            }
+        });
+        if new_ranges.len() > 0 {
+            seeds = new_ranges;
+        }
+    });
+
+    seeds.iter().map(|(s, _)| s).min().unwrap().to_string()
 }
